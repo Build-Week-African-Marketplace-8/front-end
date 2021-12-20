9398dcc1a61a6cbc489d78369a760dab687b5a6d
Chris Kelley <chriskelley85@gmail.com>
2021-12-20 16:58:55 -0600

    edit profile

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import axios from 'axios';
+
+import Item from './Item';
 
 const ItemsList = () => {
-  return <div></div>;
+  const [items,setItems] = useState([]);
+  useEffect(()=> {
+    axios.get('https://fakerapi.it/api/v1/products')
+    .then(res =>{
+      console.log(res.data.data)
+      setItems(res.data.data)
+    })
+    .catch(err => console.log(err))
+  },[])
+
+  return(
+    
+    <div className="ui grid container">
+      {items.map(item=> <div className="four wide column"><Item key={item.ean} item={item}/></div>)}
+      </div>
+  ) 
 };
 
 export default ItemsList;
