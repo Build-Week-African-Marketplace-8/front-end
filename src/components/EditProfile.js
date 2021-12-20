4bc6f4b558a564a7496b6eae644d8285782c94e5
Chris Kelley <chriskelley85@gmail.com>
2021-12-20 15:39:53 -0600

    edit profile page -- needs correct routing, username, update photo functionality

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,7 +1,26 @@
 import React from "react";
+import {Link} from 'react-router-dom'
+
+const initialProfile = {
+  username: "",
+};
 
 const EditProfile = () => {
-  return <div></div>;
+  return <div>
+    <h1>Profile Component</h1>
+    <p>$username</p>
+    <Link to='/'> 
+      <button id='edit-info'>Edit info</button> 
+    </Link>
+    links to home-- not sure how to render edit username/upload photo
+    <form>
+  <label>
+    Update username:
+    <input type="text" name="new username" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+  </div>;
 };
 
 export default EditProfile;
