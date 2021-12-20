d5bba9934409ff070e9e67469066ead61b05f7ae
Matt <86365526+matthewstack@users.noreply.github.com>
2021-12-20 14:51:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AddItem from "./components/AddItem";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import EditProfile from "./components/EditProfile";
+import Register from "./components/Register";
+import ItemsList from "./components/ItemsList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/profile" element={<EditProfile />} />
+        <Route path="/itemslist" element={<ItemsList />} />
+        <Route path="/additem" element={<AddItem />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
+
