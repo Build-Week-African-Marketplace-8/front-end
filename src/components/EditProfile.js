20c0a7c8922fa720fe70c781927a92685e587307
SheebaHashmi <sheebahashmi0@gmail.com>
2021-12-21 15:02:30 -0500

    Add upload picture functionality

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,26 +1,82 @@
-import React from "react";
+import React,{useState} from "react";
 import {Link} from 'react-router-dom'
 
 const initialProfile = {
   username: "",
+  file:'',
+  profilePicture:'',
+  isEditing: false
 };
 
 const EditProfile = () => {
-  return <div>
-    <h1>Profile Component</h1>
-    <p>$username</p>
-    <Link to='/'> 
-      <button id='edit-info'>Edit info</button> 
-    </Link>
-    links to home-- not sure how to render edit username/upload photo
-    <form>
-  <label>
-    Update username:
-    <input type="text" name="new username" />
-  </label>
-  <input type="submit" value="Submit" />
-</form>
-  </div>;
+  const [values,setValues] = useState(initialProfile);
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setValues({
+      ...values,
+      isEditing:!values.isEditing
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValues({
+      ...values,
+      isEditing: false
+    })
+    console.log(values)
+  }
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    reader.onloadend = () => {
+      setValues({
+        ...values,
+        file: file,
+        profilePicture: reader.result
+      });
+    }
+    reader.readAsDataURL(file);
+    
+  }
+  
+  return (
+    <div>
+      <h2>Username: {values.username}</h2>
+      <img className="ui medium circular image" src={values.profilePicture}/>
+      <button className="btn" onClick={handleEdit}>Edit Profile</button>
+
+      {
+        values.isEditing && <form onSubmit={handleSubmit}>
+          <label>Edit username:</label>
+          <input
+            name="username"
+            type="text"
+            value={values.username}
+            onChange={handleChange}
+          />
+
+          <label>Edit Profile picture</label>
+          <input
+            type="file"
+            name="profilePicture"
+            onChange={handleUpload}
+          />
+          <button className="btn">Submit</button>
+        </form>
+      }
+    </div>
+  )
 };
 
 export default EditProfile;
